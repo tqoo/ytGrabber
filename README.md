af7996e9ca79edc7d7802fad09f4a508b6d000b0
Comix Inc <56793799+tqoo@users.noreply.github.com>
2024-02-21 19:35:28 +0000

    Created top GUI for video downloading

---
+++ README.md
@@ -0,0 +1,2 @@
+# ytGrabber
+Grab your favourite videos and music off of Youtube. No hassle, no fuss, just click and go!
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-from pyqt import QLabel
+from PyQt6.QtWidgets import QLabel, QApplication, QWidget, QGridLayout, QLineEdit, QComboBox, QPushButton, QCheckBox
+
+app = QApplication([])
+
+class VideoWindow(QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle('PyGrabber')
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        videoURLbox = QLineEdit()
+        videoURLbox.setPlaceholderText("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        layout.addWidget(videoURLbox, 0,0)
+
+        videoFormatDropdown = QComboBox()
+        videoFormatDropdown.addItems(["mp4","mov","avi","webm"])
+        layout.addWidget(videoFormatDropdown, 0,1)
+
+        grabButton = QPushButton("Grab!")
+        layout.addWidget(grabButton, 1,0)
+
+        metadataToggle = QCheckBox("Include metadata")
+        layout.addWidget(metadataToggle, 1,1)
+
+
+        self.setFocus()
+        self.resize(485,100)
+        self.show()
+# start the event loop
+
+window = VideoWindow()
+app.exec()
